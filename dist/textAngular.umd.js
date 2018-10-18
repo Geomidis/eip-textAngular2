131c281be7155a63973f23b6156282f6c367affb
George Ioakeimidis <george.ioakeimidis@telegraph.co.uk>
2018-10-18 17:11:01 +0100

    new version 1.6 lol

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -1050,7 +1050,7 @@ angular.module('textAngularSetup', [])
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.5
+Version 1.6
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
